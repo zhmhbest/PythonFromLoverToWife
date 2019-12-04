e178148af0c55172db6a4d44e4ae31d5701f884a
ZHMH <zhmhbest@gmail.com>
2019-12-04 15:25:52 +0800

    Initial

---
+++ 02.py
@@ -0,0 +1,14 @@
+"""
+    x, y, z为随机生成的三个正整数
+    请由小到大依次输出。
+"""
+import random
+
+if __name__ == '__main__':
+    x = random.randint(0, 9)
+    y = random.randint(0, 9)
+    z = random.randint(0, 9)
+    print(x, y, z)
+    # ↓↓↓↓↓↓↓↓ 在此处编写自己的代码 ↓↓↓↓↓↓↓↓
+
+    # ↑↑↑↑↑↑↑↑ 在此处编写自己的代码 ↑↑↑↑↑↑↑↑
