abde1581012b776095e3fccef02d3872651a8af3
zhmhbest <zhmhbest@gmail.com>
2019-12-07 17:54:54 +0800

    13 15 exchange

---
+++ 15-递归.py
@@ -0,0 +1,15 @@
+"""
+    已知a_(n+1) = a_(n) * 2 + 1/a_(n)
+    a_(0) = 1
+    a_(1) = 3
+    a_(2) = 6 + 1/3
+    求a_(123)
+"""
+
+def a(n):
+    pass
+
+
+if __name__ == '__main__':
+    result = f(1)
+    print(result)
