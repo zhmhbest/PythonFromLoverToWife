db027dd5771cf1d08b42350f5fdd4f58ac1ad307
ZHMH <zhmhbest@gmail.com>
2019-12-09 16:59:44 +0800

    15

---
+++ 15-递推.py
@@ -0,0 +1,26 @@
+"""
+    已知a_(n+1) = a_(n) * 2 + 1/a_(n)
+    a_(0) = 1
+    a_(1) = 3
+    a_(2) = 6 + 1/3
+    求a_(123)
+"""
+
+
+def get_an(n, a0, fun):
+    """
+    获取递推公式第n项的值
+    :param n: 第几项
+    :param a0: a(0)的值
+    :param fun: 递推表达式
+    :return: 结果
+    """
+    result = a0
+    for i in range(1, n + 1):
+        result = fun(result)
+    # end for
+    return result
+
+
+if __name__ == '__main__':
+    print(get_an(123, 1, lambda n: n * 2 + 1 / n))
