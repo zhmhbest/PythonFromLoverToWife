9d06dcfb0c5b5087c15d4618b5e228a21425f2b8
zhmhbest <zhmhbest@gmail.com>
2019-12-10 14:41:00 +0800

    回文数

--- 12-回文数.py
+++ 12-回文数.py
@@ -1,11 +1,11 @@
 """
-    输出100~5000的所有回文数
+    输出100~50000的所有回文数
     回文数：高低位对称的数字
     eg: 12321, 1221, 988523325889
 """
 
 
-def fractured_number(num, weight=10):
+def fracture_number(num, weight=10):
     result = []
     while num != 0:
         result.append(num % weight)
@@ -15,10 +15,10 @@ def fractured_number(num, weight=10):
 
 
 def is_palindrome(num):
-    num_arr = fractured_number(num)
+    num_arr = fracture_number(num)
     num_len = len(num_arr)
-    for i in range(int(num_len / 2)):  # 长度为奇数时，中间的数没有判断的必要
-        if num_arr[i] != num_arr[num_len - i - 1]:
+    for _i_ in range(int(num_len / 2)):  # 长度为奇数时，中间的数没有判断的必要
+        if num_arr[_i_] != num_arr[num_len - _i_ - 1]:
             return False
         # end if
     # end for
