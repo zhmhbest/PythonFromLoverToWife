df01b41b8c4a2539583a795ed69f7a67431d2d08
zhmhbest <zhmhbest@gmail.com>
2019-12-20 18:10:14 +0800

    数字时钟

--- 04-数字时钟.py
+++ 04-数字时钟.py
@@ -2,12 +2,20 @@
     制作一款数字时钟
     要求：每隔一秒打印一次当前系统的时间
 """
-import time
+
+
+def delay(seconds):
+    import time
+    time.sleep(seconds)
+
+
+def get_current_time_string():
+    import time
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
 
 if __name__ == '__main__':
     while True:
-        sys_t = time.time()
-        str_t = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sys_t))
-        print(str_t)
+        print(get_current_time_string())
         # 暂停一秒
-        time.sleep(1)
+        delay(1)
